dd8f71449bac9f72d1a429acb67533c780879f67
Andrew Waller <andrewhwaller@gmail.com>
2019-03-17 22:07:33 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  counter = 0
+  while array.length > 0
+    yield
+    counter + 1
+  end
 end
+
+my_each { for item in array do return "#{item}" end }
